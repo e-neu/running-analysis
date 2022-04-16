8061c2bd3bdd98a126a66a326719d6a65fcc6173
Eugen Neu <neu-github@mailbox.org>
2022-04-16 15:02:41 +0200

    Initial commit based on cookiecutter DS template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Running analysis based on data-driven and physics-based modelling',
+    author='Eugen Neu',
+    license='MIT',
+)
